93624d6e6dd34f3d53250c3936bde1d6fb88f22d
Robert Schlee <robert.schlee@stud.th-rosenheim.de>
2026-04-21 07:57:31 +0200

    added Postgresqsl nuget package

---
+++ src/MiniJiraAspire.Server/Migrations/AppDbContextFactory.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace MiniJiraAspire.Server.Migrations;
+
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=techconfdb;Username=postgres;Password=techconf");
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
