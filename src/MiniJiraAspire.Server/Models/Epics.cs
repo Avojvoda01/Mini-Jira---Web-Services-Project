d363b1aa90344cff63b13104715c3c5908f40dcc
Robert Schlee <robert.schlee@stud.th-rosenheim.de>
2026-04-21 07:06:27 +0200

    created model for epics

---
+++ src/MiniJiraAspire.Server/Models/Epics.cs
@@ -0,0 +1,10 @@
+namespace MiniJiraAspire.Server.Models;
+
+public class Epics
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    // TODO: Add references for Tasks
+    
+}
