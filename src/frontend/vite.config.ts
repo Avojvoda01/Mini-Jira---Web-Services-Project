3d96828cc0bea8727765d4eed7c41cbfce0631fd
Quirin Brecheisen <quirin.brecheisen@stud.th-rosenheim.de>
2026-04-25 13:56:51 +0200

    refactor: update Vite configuration to load environment variables for API target

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,24 +1,29 @@
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(fileURLToPath(new URL('.', import.meta.url)), './src')
-    }
-  },
-  server: {
-    proxy: {
-      // Proxy API calls to the app service
-      '/api': {
-        target: process.env.SERVER_HTTPS || process.env.SERVER_HTTP,
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const apiTarget = env.SERVER_HTTPS || env.SERVER_HTTP || 'http://localhost:5413';
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(fileURLToPath(new URL('.', import.meta.url)), './src')
+      }
+    },
+    server: {
+      proxy: {
+        // Proxy API calls to the app service
+        '/api': {
+          target: apiTarget,
+          changeOrigin: true
+        }
       }
     }
-  }
+  };
 });
