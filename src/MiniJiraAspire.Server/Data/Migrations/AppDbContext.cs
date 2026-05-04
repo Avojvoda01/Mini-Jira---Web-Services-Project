7e6fc3517c4a4babaac7162dcb85eb3c80b8c01a
Josef Kaffl <j.kaffl@techdivision.com>
2026-05-04 16:36:11 +0200

    Feature-Project-Endpoint: Fix AppDb Context

--- src/MiniJiraAspire.Server/Data/Migrations/AppDbContext.cs
+++ src/MiniJiraAspire.Server/Data/Migrations/AppDbContext.cs
@@ -6,12 +6,9 @@ using Microsoft.EntityFrameworkCore;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<Epic> Epics => Set<Epic>();
-<<<<<<< HEAD
     public DbSet<Project> Projects => Set<Project>();
-=======
     public DbSet<Comment> Comments => Set<Comment>();
     //public DbSet<User> Users => Set<User>();
->>>>>>> main
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
