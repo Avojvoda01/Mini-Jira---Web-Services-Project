375c40c8a5517c87702c0d99c3ba33ed47a9b670
QuirinB <quirin.brecheisen@stud.th-rosenheim.de>
2026-05-16 13:56:07 +0200

    add new db model ProjectMember and update endpoints to allow assign,remove members and update existing logic

--- src/MiniJiraAspire.Server/Features/Project/Commands/AddProjectMemberCommand.cs
+++ src/MiniJiraAspire.Server/Features/Project/Commands/AddProjectMemberCommand.cs
@@ -1,11 +1,18 @@
 using MediatR;
+using MiniJiraAspire.Server.Persistence.Repositories;
 
 namespace MiniJiraAspire.Server.Features.Project.Commands;
 
 public record AddProjectMemberCommand(string ProjectId, string UserId, string Role) : IRequest;
 
-public class AddProjectMemberHandler : IRequestHandler<AddProjectMemberCommand>
+public class AddProjectMemberHandler(IProjectRepository repository) : IRequestHandler<AddProjectMemberCommand>
 {
-    public Task Handle(AddProjectMemberCommand request, CancellationToken ct)
-        => Task.CompletedTask;
+    public async Task Handle(AddProjectMemberCommand request, CancellationToken ct)
+    {
+        await repository.AddMemberAsync(
+            Guid.Parse(request.ProjectId),
+            Guid.Parse(request.UserId),
+            request.Role,
+            ct);
+    }
 }
