ba1f003b3cb39ca55d32adc4ada8b8671f194dde
Robert Schlee <robert.schlee@stud.th-rosenheim.de>
2026-06-03 11:43:37 +0200

    Included Imports in Test Project

--- src/MiniJira.Test/Features/Epic/UpdateEpicHandlerTests.cs
+++ src/MiniJira.Test/Features/Epic/UpdateEpicHandlerTests.cs
@@ -1,5 +1,6 @@
 using Moq;
 using MiniJiraAspire.Server.Features.Epic.Commands;
+using MiniJiraAspire.Server.Models;
 using MiniJiraAspire.Server.Persistence.Repositories;
 namespace MinirJira.Test.Features.Epic;
 public class UpdateEpicHandlerTests
